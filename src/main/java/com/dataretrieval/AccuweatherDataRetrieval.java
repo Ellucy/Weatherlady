2307f85e62fb09588bb7bb9a590bd427bf1fe818
Eleri Mets <eleri.apsolon@gmail.com>
2024-02-06 10:02:04 +0200

    Retrieve example data from AccuWeather using Apache HttpClient

---
+++ src/main/java/com/dataretrieval/AccuweatherDataRetrieval.java
@@ -0,0 +1,45 @@
+package com.dataretrieval;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class AccuweatherDataRetrieval {
+
+    public static void main(String[] args) {
+
+        String postmanQueryCityUrl = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=&q=tallinn";
+        String postmanQueryCityWeatherUrl = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/127964?apikey=";
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpGet getRequest = new HttpGet(postmanQueryCityWeatherUrl);
+
+        try {
+            HttpResponse response = httpClient.execute(getRequest);
+
+            if (response.getStatusLine().getStatusCode() == 200) {
+
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(response.getEntity().getContent()));
+
+                String line;
+                StringBuilder result = new StringBuilder();
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);
+                }
+
+                System.out.println("Response: " + result.toString());
+            } else {
+                System.err.println("Failed to retrieve data. HTTP Error Code: " +
+                        response.getStatusLine().getStatusCode());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
