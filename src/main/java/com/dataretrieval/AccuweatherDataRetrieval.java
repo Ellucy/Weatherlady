47a7da26dfde3e767f9b839d55e3d4bba913f0a4
Eleri Mets <eleri.apsolon@gmail.com>
2024-02-09 13:41:43 +0200

    Create Location and Weather entity classes to represent location and weather data; Create Hibernate configuration file hibernate.cfg.xml; Implement two distinct queries for retrieving weather data from Accuweather, as query to retrieve weather data needs location key; Implement LocationDetails class for detailed location information; Use LocationDetails inside AccuweatherLocationHandler; Create downloadAndSaveWeatherData method inside AccuweatherDataRetrieval to save weather data to database.

--- src/main/java/com/dataretrieval/AccuweatherDataRetrieval.java
+++ src/main/java/com/dataretrieval/AccuweatherDataRetrieval.java
@@ -1,27 +1,64 @@
 package com.dataretrieval;
 
+import com.entities.Weather;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
 
 public class AccuweatherDataRetrieval {
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+        sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Weather.class)
+                .buildSessionFactory();
+    }
 
     public static void main(String[] args) {
 
         String apiKey = System.getenv("AW_API_KEY");
+        System.out.println(apiKey);
 
-        String accuweatherResponse = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/349727?apikey=" + apiKey + "&details=true";
+        try {
+            downloadAndSaveWeatherData(apiKey);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private static void downloadAndSaveWeatherData(String apiKey) throws IOException {
+
+        String requestedCity = "Tallinn";
+        String transformedInput = requestedCity.toLowerCase().replaceAll("\\s+", "");
+        System.out.println(transformedInput);
+
+        LocationDetails locationDetails = AccuweatherLocationHandler.getLocationDetails(apiKey, transformedInput);
+
+        String locationKey = locationDetails.getLocationKey();
+
+        String accuweatherOneDayUrl = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/" + locationKey + "?apikey=" + apiKey + "&details=true";
+        String accuweatherFiveDaysUrl = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/349727?apikey=" + apiKey + "&details=true";
         CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-        HttpGet getRequest = new HttpGet(accuweatherResponse);
+        HttpGet getRequest = new HttpGet(accuweatherOneDayUrl);
 
         try {
             HttpResponse response = httpClient.execute(getRequest);
+
+            System.out.println(response);
 
             if (response.getStatusLine().getStatusCode() == 200) {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
@@ -34,10 +71,6 @@ public class AccuweatherDataRetrieval {
                 // Parse JSON response
                 JSONObject jsonResponse = new JSONObject(result.toString());
 
-                // Headline data
-                JSONObject headline = jsonResponse.getJSONObject("Headline");
-                System.out.println("Headline: " + headline.getString("Text"));
-
                 // DailyForecasts data
                 // Information for today, the first day in array (index 0)
                 JSONObject forecast = jsonResponse.getJSONArray("DailyForecasts").getJSONObject(0);
@@ -47,28 +80,54 @@ public class AccuweatherDataRetrieval {
                 double minTemp = temperature.getJSONObject("Minimum").getDouble("Value");
                 double maxTemp = temperature.getJSONObject("Maximum").getDouble("Value");
                 double averageTemp = (minTemp + maxTemp) / 2;
-                System.out.println("Average Temperature: " + averageTemp);
 
                 // Relative humidity data
                 JSONObject relativeHumidity = forecast.getJSONObject("Day").getJSONObject("RelativeHumidity");
-                int minRH = relativeHumidity.getInt("Minimum");
-                int maxRH = relativeHumidity.getInt("Maximum");
                 int averageRH = relativeHumidity.getInt("Average");
-                System.out.println("Average Relative Humidity: " + averageRH);
 
                 // Wind data
                 JSONObject wind = forecast.getJSONObject("Day").getJSONObject("Wind");
                 double windSpeed = wind.getJSONObject("Speed").getDouble("Value");
                 int windDirectionDegrees = wind.getJSONObject("Direction").getInt("Degrees");
                 String windDirectionLocalized = wind.getJSONObject("Direction").getString("Localized");
-                System.out.println("Wind Speed: " + windSpeed + " mph");
-                System.out.println("Wind Direction: " + windDirectionLocalized + " (" + windDirectionDegrees + "°)");
+
+                // Parse and extract date
+                String dateString = forecast.getString("Date");
+                Date date = new SimpleDateFormat("yyyy-MM-dd").parse(dateString);
+
+                // Create Weather entity
+                Weather weather = new Weather();
+                weather.setCountryName(locationDetails.getCountryName());
+                weather.setRegionName(locationDetails.getRegionName());
+                weather.setCityName(locationDetails.getCityName());
+                weather.setLatitude(locationDetails.getLatitude());
+                weather.setLongitude(locationDetails.getLongitude());
+                weather.setDate(date);
+                weather.setTemperature(averageTemp);
+                weather.setHumidity(averageRH);
+                weather.setWindDirection(windDirectionLocalized + " (" + windDirectionDegrees + "°)");
+                weather.setWindSpeed(windSpeed);
+
+                // Save Weather entity to the database
+                saveWeatherData(weather);
             } else {
                 System.err.println("Failed to retrieve data. HTTP Error Code: " + response.getStatusLine().getStatusCode());
             }
-
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void saveWeatherData(Weather weather) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Weather managedWeather = (Weather) session.merge(weather);
+            session.merge(managedWeather);
+
+            transaction.commit();
+            System.out.println("Weather data saved successfully!");
+        } catch (Exception e) {
+            System.out.println("Failed to save weather data. Error: " + e.getMessage());
         }
     }
 }
---
+++ src/main/java/com/dataretrieval/AccuweatherLocationHandler.java
@@ -0,0 +1,68 @@
+package com.dataretrieval;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class AccuweatherLocationHandler {
+
+    public static LocationDetails getLocationDetails(String apiKey, String requestedCity) throws IOException {
+
+        //String accuweatherCityUrl = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=" + apiKey + "&q=" + transformedInput;
+        String endpoint = "http://dataservice.accuweather.com/locations/v1/cities/search";
+        String params = String.format("apikey=%s&q=%s", apiKey, requestedCity);
+        String urlString = String.format("%s?%s", endpoint, params);
+
+        // Open connection
+        URL url = new URL(urlString);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        try {
+            // Read response
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+
+            // Parse JSON response
+            JSONArray jsonArray = new JSONArray(response.toString());
+            JSONObject jsonObject = jsonArray.getJSONObject(0);
+
+            // Extract relevant information
+            String locationKey = jsonObject.getString("Key");
+            String cityName = jsonObject.getString("EnglishName");
+            JSONObject regionObject = jsonObject.getJSONObject("Region");
+            String regionName = regionObject.getString("EnglishName");
+            JSONObject countryObject = jsonObject.getJSONObject("Country");
+            String countryName = countryObject.getString("EnglishName");
+            JSONObject geoPositionObject = jsonObject.getJSONObject("GeoPosition");
+            double latitude = geoPositionObject.getDouble("Latitude");
+            double longitude = geoPositionObject.getDouble("Longitude");
+
+            // Print extracted information
+            System.out.println("City Name: " + cityName);
+            System.out.println("Region Name: " + regionName);
+            System.out.println("Country Name: " + countryName);
+            System.out.println("Latitude: " + latitude);
+            System.out.println("Longitude: " + longitude);
+
+            // Return location key
+            return new LocationDetails(locationKey, cityName, regionName, countryName, latitude, longitude);
+        } finally {
+            connection.disconnect();
+        }
+    }
+}
