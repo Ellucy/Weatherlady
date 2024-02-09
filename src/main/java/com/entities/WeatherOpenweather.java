763640d59ac915aaed00a5773d231a3b096e7881
AngieSepulveda <angiemischelesepulvedamonroy@gmail.com>
2024-02-09 17:08:18 +0200

    Save data from Openweather with Hibernate

---
+++ src/main/java/com/entities/WeatherOpenweather.java
@@ -0,0 +1,50 @@
+package com.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name= "weatheropenweather")
+
+public class WeatherOpenweather {
+
+        @Id
+        @GeneratedValue
+        private UUID weatherId;
+
+        @Column(name = "country")
+        private String country;
+
+        @NotNull(message = "City name cannot be null")
+        @Column(name = "city_name")
+        private String cityName;
+
+        @Column(name = "latitude")
+        private Double latitude;
+
+        @Column(name = "longitude")
+        private Double longitude;
+
+
+        @Column(name = "temperature")
+        private Double temperature;
+
+        @Column(name = "pressure")
+        private Integer pressure;
+
+        @Column(name = "humidity")
+        private Integer humidity;
+
+
+        @Column(name = "wind_speed")
+        private Double windSpeed;
+}
