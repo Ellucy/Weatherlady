e10cdd7e782cbfa41bec33534954d8b6aa1398b3
Eleri Mets <eleri.apsolon@gmail.com>
2024-02-12 20:24:16 +0200

    Add the WireMock dependency to pom.xml file to set up a mock server. Create new skeleton file to test APIConnection class.

---
+++ src/test/java/com/retrievedata/APIConnectionTest.java
@@ -0,0 +1,22 @@
+package com.retrievedata;
+
+import com.retrievedata.APIConnection;
+import junit.framework.TestCase;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class APIConnectionTest {
+
+    @Test
+    public void downloadWeatherDataSuccess() {
+
+    }
+
+    @Test
+    public void downloadWeatherDataFailure() {
+
+    }
+
+}
