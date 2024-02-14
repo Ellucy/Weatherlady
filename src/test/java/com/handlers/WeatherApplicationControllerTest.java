a1cf639ee5659ce4a28475854a756297bc36379b
Eleri Mets <eleri.apsolon@gmail.com>
2024-02-14 10:59:21 +0200

    Add Mockito mock objects library core API to pom.xml

--- src/test/java/com/handlers/WeatherApplicationControllerTest.java
+++ src/test/java/com/handlers/WeatherApplicationControllerTest.java
@@ -1,13 +1,22 @@
 package com.handlers;
 
+import com.retrievedata.AccuweatherDataRetrieval;
+import com.retrievedata.OpenweatherDataRetrieval;
+import com.retrievedata.WeatherstackDataRetrieval;
 import org.junit.jupiter.api.Test;
 import com.handlers.WeatherApplicationController;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class WeatherApplicationControllerTest {
 
+    @Test
     public void testAddingNaturalDisaster() {
+        AccuweatherDataRetrieval accuweatherDataRetrieval = mock(AccuweatherDataRetrieval.class);
+        OpenweatherDataRetrieval openweatherDataRetrieval = mock(OpenweatherDataRetrieval.class);
+        WeatherstackDataRetrieval weatherstackDataRetrieval = mock(WeatherstackDataRetrieval.class);
+
     }
 
     public void testGetDisastersByDate() {
