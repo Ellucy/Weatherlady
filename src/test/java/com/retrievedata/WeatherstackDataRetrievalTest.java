3200337849044b8f2ab6dd2d4b2328b4e686658d
Eleri Mets <eleri.apsolon@gmail.com>
2024-02-14 18:26:29 +0200

    Add Mockito as dependency to pom.xml. Update WireMock version.

---
+++ src/test/java/com/retrievedata/WeatherstackDataRetrievalTest.java
@@ -0,0 +1,20 @@
+package com.retrievedata;
+
+import com.retrievedata.WeatherstackDataRetrieval;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+public class WeatherstackDataRetrievalTest {
+
+    public void testDownloadAndSetWeatherData() {
+    }
+}
